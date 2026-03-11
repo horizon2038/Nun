5a2864e2f0e3ebf769a234793b8d53fcd1076813
horizon <rekka728@gmail.com>
2026-03-12 02:08:35 +0900

    [clean] remove unused comments

--- src/arch/x86_64/benchmark.rs
+++ src/arch/x86_64/benchmark.rs
@@ -5,20 +5,6 @@ use crate::types::*;
 
 #[inline(always)]
 pub fn cycle_counter() -> Word {
-    /*
-    let eax: Word;
-    let edx: Word;
-    unsafe {
-        asm!(
-            "rdtscp",
-            out("eax") eax,
-            out("edx") edx,
-            lateout("ecx") _,
-            options(nomem, nostack, preserves_flags),
-        );
-    }
-    (((edx as Word) << 32) | (eax as Word)) as Word
-    */
     let mut _aux = 0u32;
     unsafe { __rdtscp(&mut _aux) as Word }
 }
